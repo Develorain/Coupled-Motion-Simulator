ad92f9904e24c019c1e5c04f3443979de9431534
Develorain <feoamnfgoamngdasd@users.noreply.github.com>
2017-01-08 15:55:43 -0500

    Refactored Vectors in DanglingBoxModel

--- src/com/ahmad/Models/ModeOne/DanglingBoxModel.java
+++ src/com/ahmad/Models/ModeOne/DanglingBoxModel.java
@@ -2,7 +2,6 @@ package com.ahmad.Models.ModeOne;
 
 import com.ahmad.Models.BoxModel;
 import com.ahmad.Tools.Constants;
-import com.ahmad.Tools.Vector;
 
 public class DanglingBoxModel extends BoxModel {
     private SystemModelModeOne systemModelModeOne;
@@ -15,35 +14,27 @@ public class DanglingBoxModel extends BoxModel {
         calculateStartingPositionCoordinates();
     }
 
-    public void updateMass(double tension, double acceleration) {
-        mass = tension / (Constants.GRAVITY - acceleration);
-    }
-
     @Override
     public void calculateStartingPositionCoordinates() {
-        topLeftCorner = Vector.createFromCartesian(
-                systemModelModeOne.pulley.topLeftCorner.getX() + systemModelModeOne.pulley.radius,
-                systemModelModeOne.pulley.topLeftCorner.getY() + systemModelModeOne.pulley.diameter
-        );
+        topLeftCorner.setX(systemModelModeOne.pulley.topLeftCorner.getX() + systemModelModeOne.pulley.radius);
+        topLeftCorner.setY(systemModelModeOne.pulley.topLeftCorner.getY() + systemModelModeOne.pulley.diameter);
 
         calculateBoxVerticesFromTopLeft();
     }
 
     @Override
     public void calculateBoxVerticesFromTopLeft() {
-        topRightCorner = Vector.createFromCartesian(
-                (int) (topLeftCorner.getX() + boxWidth),
-                (int) (topLeftCorner.getY())
-        );
+        topRightCorner.setX((int) (topLeftCorner.getX() + boxWidth));
+        topRightCorner.setY((int) (topLeftCorner.getY()));
 
-        bottomLeftCorner = Vector.createFromCartesian(
-                (int) (topLeftCorner.getX()),
-                (int) (topLeftCorner.getY() + boxHeight)
-        );
+        bottomLeftCorner.setX((int) (topLeftCorner.getX()));
+        bottomLeftCorner.setY((int) (topLeftCorner.getY() + boxHeight));
 
-        bottomRightCorner = Vector.createFromCartesian(
-                (int) (topLeftCorner.getX() + boxWidth),
-                (int) (topLeftCorner.getY() + boxHeight)
-        );
+        bottomRightCorner.setX((int) (topLeftCorner.getX() + boxWidth));
+        bottomRightCorner.setY((int) (topLeftCorner.getY() + boxHeight));
+    }
+
+    public void updateMass(double tension, double acceleration) {
+        mass = tension / (Constants.GRAVITY - acceleration);
     }
 }
